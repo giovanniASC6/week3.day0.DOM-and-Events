a95df36e3df8885c145adf638aa76c7696fc4dda
giovanniASC6 <giovanni.owens@gmail.com>
2019-07-29 11:39:36 -0400

    DOM-and-Events

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,9 @@
+const rightSideElement = document.getElementsByClassName("right_side")
+rightSideElement[0].style.backgroundColor = "#d3c26f";
+
+const anyName = document.getElementsByClassName("name")
+for (let i = 0; i < anyName.length; i++){
+    anyName[i].style.textShadow = "-6px 5px 2px yellow"
+}
+
+
