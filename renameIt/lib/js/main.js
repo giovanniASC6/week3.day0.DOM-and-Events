c95985b6f098aaa0ead045dfba4757c43990d580
giovanniASC6 <giovanni.owens@gmail.com>
2019-07-29 12:03:21 -0400

    DOM and Events

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,7 @@
+const sword = document.getElementById("item_name")
+sword.innerHTML = "Frostfang";
+
+
+const ror = document.getElementById("item_Img").src 
+ror= "renameIt/assets/Frostfang_item.png"
+
